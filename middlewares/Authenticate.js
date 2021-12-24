089d7f817aaa474f28ff541197e0b6546fbb6157
Benjamin Nguyen <benjamin.nguyen@contemi.com>
2021-12-24 10:35:05 +0700

    Add logic of activating new account has already created

--- middlewares/Authenticate.js
+++ middlewares/Authenticate.js
@@ -1,14 +1,11 @@
-const config = require('./../config/config');
-const Authorize = require('./Authorize');
-const jwt = require("jsonwebtoken");
+const VerifyActivate = require('./VerifyActivate');
 
 const Authenticate = (req, res, next) => {
   if (req.isAuthenticated()) {
-    res.locals.isAdmin = req.user.admin;
-    return next();
-  } else {
-    return res.redirect('/login');
+    return VerifyActivate(req, res, next);
   }
+
+  return res.redirect('/login');
 };
 
 module.exports = Authenticate;
--- middlewares/VerifyAdmin.js
+++ middlewares/VerifyAdmin.js
@@ -1,14 +1,11 @@
-const config = require('./../config/config');
-const Authorize = require('./Authorize');
-const jwt = require("jsonwebtoken");
+const VerifyActivate = require('./VerifyActivate');
 
 const VerifyAdmin = (req, res, next) => {
 	if (req.isAuthenticated() && req.user.admin) {
-		res.locals.isAdmin = req.user.admin;
-		return next();
-	} else {
-		return res.send("403 Not Forbidden - Yêu cầu quyền truy cập vào tài nguyên này.");
+		return VerifyActivate(req, res, next);
 	}
+
+	return res.send("403 Not Forbidden - Yêu cầu quyền truy cập vào tài nguyên này.");
 };
 
 module.exports = VerifyAdmin;
