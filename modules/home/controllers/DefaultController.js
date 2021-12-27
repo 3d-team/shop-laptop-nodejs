cb3987c7e44041fb09c46081a51f43bbda26d771
Benjamin Nguyen <benjamin.nguyen@contemi.com>
2021-12-27 22:13:30 +0700

    Restore some missing code from recent merge request

--- modules/home/controllers/DefaultController.js
+++ modules/home/controllers/DefaultController.js
@@ -4,14 +4,7 @@ var menu = require('../../common_model/MenuContent');
 class DefaultController {
 	
 	index(req, res) {
-<<<<<<< HEAD
 		
-=======
-		console.log(__dirname);
-		// app.locals.Cart.number = req.app.locals.CartSize;
->>>>>>> main
-		console.log(__dirname);
-		// app.locals.Cart.number = req.app.locals.CartSize;
 		res.render('index', {
 			content: "Default: index",		
 			menuContent: menu.getContentHomeMenuItem()
